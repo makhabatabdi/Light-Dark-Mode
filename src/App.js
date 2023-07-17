2512f4f5898ca841ca50527a2497ead1723fb68b
makhabatabd <makhabat9898@gmail.com>
2023-07-17 14:00:17 -0700

    Работа без логики

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="toggle-container">
+        <p className="change-text">Click here</p>
+        <input type="checkbox" id="toggle-btn" />
+        <label htmlFor="toggle-btn" className="toggle-label"></label>
+      </div>
+
+      <div className="container">
+        <div id="sun" className="shape"></div>
+        <div id="moon" className="shape"></div>
+      </div>
     </div>
   );
 }
-
-export default App;
